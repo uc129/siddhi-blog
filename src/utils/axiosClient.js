8ffdcd06fd591e0d46229674e69a1d16f39952d9
Utkarsh Chaudhary <utkarshchaudhary12@gmail.com>
2024-01-29 21:49:16 +1100

    cors

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://blog-server.eba-9zeg5kgm.us-west-2.elasticbeanstalk.com/api/v1',
+    baseURL: 'http://blog-server.eba-9zeg5kgm.us-west-2.elasticbeanstalk.com/api/v1',
     headers: {
         'Content-Type': 'multipart/form-data'
     }
